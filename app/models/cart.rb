8c90ef7ef17a36e82c80b99ac59a2e45189d0de1
benser1 <briganenser28@gmail.com>
2016-02-18 14:01:16 -0700

    cart #total and #add_item working

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,6 +5,25 @@ class Cart < ActiveRecord::Base
    belongs_to :order
    has_one :status, through: :order
 
-   def add_item(item)
+   def total
+      # line_items.quantity * items.price
+      line_items.collect do |line_item| 
+         line_item.quantity * line_item.item.price
+      end.inject(:+)
    end
+
+   def add_item(item)
+      new_item = line_items.find_by(id: item)
+
+      if new_item
+         new_item.quantity += 1
+      else 
+         new_item = self.line_items.build(item_id: item)
+         new_item.quantity = 1
+      end
+      new_item
+   end ## add item end
+
+
+
 end #ends class
